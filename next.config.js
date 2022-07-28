ad07eed81033dc8213da1d273c94d173c4dda249
Bruno Afonso Lobo <bno.afonso@hotmail.com>
2022-07-28 16:26:08 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  async redirects(){
+    return[
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
+
